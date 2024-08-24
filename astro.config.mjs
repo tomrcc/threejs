c6acebb325f72310e5af24d4752aa41f13c49a93
tomrcc <tom@cloudcannon.com>
2024-08-25 11:57:00 +1200

    Clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import bookshop from '@bookshop/astro-bookshop';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import alpine from '@astrojs/alpinejs';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import bookshop from "@bookshop/astro-bookshop";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tiny-jackal.cloudvent.net/',
-  integrations: [react(), tailwind(), bookshop(), alpine(), mdx()],
+  site: "https://tiny-jackal.cloudvent.net/",
+  integrations: [react(), tailwind(), bookshop()],
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -12,37 +12,14 @@ const seoSchema = z
   })
   .optional();
 
-const blogCollection = defineCollection({
+const pagesCollection = defineCollection({
   schema: z.object({
-    date: z.string().or(z.date()),
     title: z.string(),
-    tags: z.array(z.string()),
-    author: z.string(),
-    thumb_image_path: z.string(),
-    thumb_image_alt: z.string(),
-    image: z.string(),
-    image_alt: z.string(),
+    content_blocks: z.array(z.any()),
     seo: seoSchema,
   }),
 });
 
-const pageSchema = z.object({
-  title: z.string(),
-  content_blocks: z.array(z.any()),
-  seo: seoSchema,
-});
-
-const paginatedCollectionSchema = z.object({
-  title: z.string(),
-  page_size: z.number().positive(),
-  seo: seoSchema,
-});
-
-const pagesCollection = defineCollection({
-  schema: z.union([paginatedCollectionSchema, pageSchema]),
-});
-
 export const collections = {
-  blog: blogCollection,
   pages: pagesCollection,
 };
